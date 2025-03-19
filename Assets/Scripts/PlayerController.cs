307a7970511689ef73dd13f299540fdcf26aeeca
Alan Laboy <60626253+Alan-Laboy@users.noreply.github.com>
2025-03-19 01:12:42 -0400

    Included sprites, updates script to better match the arm to the body

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,33 +7,45 @@ public class PlayerController : MonoBehaviour {
     public GameObject player;
     private bool selected = false;
     private Vector3 mousePos;
-    private float angle;
+    private Vector3 center;
+    private float angle = -45;
     private float radius;
 
     // Start is called before the first frame update
     void Start() {
         // Find radius of the arm
-        radius = Mathf.Sqrt(Mathf.Pow(player.transform.position.x - transform.position.x, 2) + Mathf.Pow(player.transform.position.y - transform.position.y, 2));
+        center = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        radius = Mathf.Sqrt(Mathf.Pow(center.x - transform.position.x, 2) + Mathf.Pow(center.y - transform.position.y, 2));
+        transform.localEulerAngles = new Vector3(0, 0, angle);
+        // Make sure that the radius is consistent
+        float xpos = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+        float ypos = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+        transform.localPosition = new Vector3(center.x + xpos, center.y + ypos, 0);
     }
 
     // Update is called once per frame
     void Update() {
         if (selected) {
 
-            //Get the angle btw the mouse and the player
+            // Get the angle between the mouse and the player
             mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-            mousePos = mousePos - player.transform.position;
+            mousePos = mousePos - center;
             angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+            // Restrict the angle to 60 degress up and down
             if (angle < -60.0f) angle = -60.0f;
             if (angle > 60.0f) angle = 60.0f;
             transform.localEulerAngles = new Vector3(0, 0, angle);
+
+            // Make sure that the radius is consistent
             float xpos = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
             float ypos = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
-            transform.localPosition = new Vector3(player.transform.position.x + xpos, player.transform.position.y + ypos, 0);
+            transform.localPosition = new Vector3(center.x + xpos, center.y + ypos, 0);
         }
     }
 
+    // Keep track on if the Arm is selected or not
     private void OnMouseDown() {
         selected = true;
     }
